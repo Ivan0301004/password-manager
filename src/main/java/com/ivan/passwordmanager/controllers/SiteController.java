90579a688e836b0af572a1082279600deea6b75b
Ivan0301004 <ignizbones@gmail.com>
2024-04-10 11:34:37 -0600

    Site controller works

--- src/main/java/com/ivan/passwordmanager/controllers/SiteController.java
+++ src/main/java/com/ivan/passwordmanager/controllers/SiteController.java
@@ -1,7 +1,16 @@
 package com.ivan.passwordmanager.controllers;
 
+import com.ivan.passwordmanager.service.SiteService;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SiteController {
+
+    private final SiteService siteService;
+
+    public SiteController(SiteService siteService) {
+        this.siteService = siteService;
+
+
+    }
 }
