00639001a01302b729fce7de08efe6142869b181
Ivan0301004 <ignizbones@gmail.com>
2024-04-10 11:34:45 -0600

    User controller works

--- src/main/java/com/ivan/passwordmanager/controllers/UserController.java
+++ src/main/java/com/ivan/passwordmanager/controllers/UserController.java
@@ -1,6 +1,7 @@
 package com.ivan.passwordmanager.controllers;
 
 import com.ivan.passwordmanager.dto.UserDto;
+import com.ivan.passwordmanager.model.User;
 import com.ivan.passwordmanager.service.impl.UserServiceImpl;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -25,11 +26,30 @@ public class UserController {
         return ResponseEntity.ok(this.userService.getAllUsers(pageable));
     }
 
-    @DeleteMapping("users/{userId}")
+    @DeleteMapping("/users/{userId}")
     public ResponseEntity<?> removeUserById(@PathVariable Long userId) {
         return ResponseEntity.ok(this.userService.removeUserById(userId));
     }
 
+    @PutMapping("/users/{userId}")
+    public ResponseEntity<User> updateUser(@PathVariable long userId, @RequestBody UserDto userDto) {
+        return new ResponseEntity<>(this.userService.updateUserById(userId, userDto), HttpStatus.ACCEPTED);
+    }
 
+    @PatchMapping("/users/{userId}")
+    public ResponseEntity<User> patchUser(@PathVariable long userId, @RequestBody Map<String, Object> fields) {
+        return new ResponseEntity<>(this.userService.patchUserById(userId, fields), HttpStatus.ACCEPTED);
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        return ResponseEntity.ok(this.userService.createUser(user));
+    }
+
+
+    @GetMapping("/users/{userId}")
+    public ResponseEntity<UserDto> getUserById(@PathVariable Long userId) {
+        return ResponseEntity.ok(this.userService.getUserById(userId));
+    }
 
 }
